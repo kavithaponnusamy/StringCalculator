d5c73cd1d25fd3e7d95219780ba0d615d0e83e72
Kavitha <kavithaa434@gmail.com>
2020-08-11 23:37:21 -0400

    second commit

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,23 +1,76 @@
+import java.util.InputMismatchException;
 
 public class StringCalculator {
 	public static void main(String[] args) {
-		System.out.println(add(""));
-		
+		System.out.println(add("1,2,3,4,5,"));
 	}
 	public static String add(String numbers) {
-		if(numbers.length()>0) {
-		String[] number=numbers.split("\n//,");
-		double sum=0;
+		if(numbers.isEmpty()) {
+				return "0";		
+		} else {
+		String delimiter=",";//default delimiter is comma.
+		
+		// For different delimiters:
+		if(numbers.startsWith("//")){
+			String temp="";
+			delimiter=numbers.substring(2,numbers.indexOf("\n"));
+			//System.out.println("delimiter"+delimiter);
+			numbers=numbers.substring(numbers.indexOf("\n")+1);
+			//System.out.println(numbers);
+			boolean repeatedDelimiter = true;
+			if (delimiter.length()>1) { 
+				temp = delimiter.substring(0,1);
+				//System.out.println("temp:"+temp);
+				for (int i=1;i<delimiter.length();i++) {
+					if (!(delimiter.substring(i,i+1).equalsIgnoreCase(temp))) {
+						repeatedDelimiter = false;
+					}
+					//System.out.println(i);
+					//System.out.println(repeatedDelimiter);
+				}
+			}
+			//System.out.println(repeatedDelimiter);
+			if (!repeatedDelimiter) {
+				for (int i=1;i<delimiter.length();i++) {
+					String sn = delimiter.substring(i,i+1);
+					//System.out.println("sn:"+sn);
+					//System.out.println("temp:"+temp);
+					numbers = numbers.replace(sn, temp);
+					}
+				delimiter = temp;
+			}
+
+			//System.out.println(delimiter);
+			//System.out.println(numbers);
+		}
+		numbers=numbers.replace("\n",delimiter); //replacing new line character with delimiter.
+		delimiter = delimiter.replace("*", "\\*");//adding escape character for *
+if (numbers.substring(numbers.length()-1).equalsIgnoreCase(delimiter)) {
+	return "Number expected but EOF found.";
+}
+		String[] number=numbers.split(delimiter);// splitting the values
+		//System.out.println("delimiter:"+delimiter);
+		int sum=0;
+		boolean neg=false;
+		String negative="Negatives not allowed: ";
 		for(String num : number) {
-			sum+=Double.parseDouble(num);
-			
+			//System.out.println("num:"+num);
+			if(Integer.parseInt(num)<0) {
+				negative+=num+",";
+				neg = true;
+			}
+			if(Integer.parseInt(num)<=1000) {
+				sum+=Integer.parseInt(num);
+			}
+		}
+		if (neg) {
+			String str= negative.substring(0,negative.length()-1);
+			//System.out.println(str);
+			//throw new InputMismatchException(str);
+			return str;
 		}
 		return String.valueOf(sum);
 		}
-		else
-		{
-			return "0";		
-		}
 	}
-
 }
+	
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -5,23 +5,59 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorTest {
 	
 	@Test
-	void returnZero() {
+	void emptyString() {
 		String number="";
 		String actual=StringCalculator.add(number);
 		assertEquals("0",actual);
 		
 	}
 	@Test
-	void unknownNumberOfArguments() {
-		String number="1,4,5.5,7";
+	void unknownAmountOfNumbers() {
+		String number="1,4,5,5,7";
 		String actual=StringCalculator.add(number);
-		assertEquals("17.5",actual);
+		assertEquals("22",actual);
 	}
 	@Test
 	void newlineSeparator() {
-		String number="1\n4,5.5,7";
+		String number="1\n2,3";
 		String actual=StringCalculator.add(number);
 		assertEquals("6",actual);
+	}
+	@Test
+	void differentDelimiter() {
+		String number="//;\n1;2";
+		String actual=StringCalculator.add(number);
+		assertEquals("3",actual);
+	}
+	@Test
+	void negativesNumbers() {
+		String number="1,2,-3,-4";
+		String actual=StringCalculator.add(number);
+		assertEquals("Negatives not allowed: -3,-4",actual);
+	}
+	@Test
+	void greaterthan1000() {
+		String number="1,1002,3,4";
+		String actual=StringCalculator.add(number);
+		assertEquals("8",actual);
+	}
+	@Test
+	void delimiterAnyLength() {
+		String number="//***\n1***2***3";
+		String actual=StringCalculator.add(number);
+		assertEquals("6",actual);
+	}
+	@Test
+	void multipleDelimiters() {
+		String number="//*%\n1*2%3";
+		String actual=StringCalculator.add(number);
+		assertEquals("6",actual);
+	}
+	@Test
+	void missingNumberInLastPosition() {
+		String number="1,2,3,4,5,";
+		String actual=StringCalculator.add(number);
+		assertEquals("Number expected but EOF found.",actual);
 	}
 	
 
